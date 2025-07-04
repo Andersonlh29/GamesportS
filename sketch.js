24f720ce5267c3d1e26f5c57b0450dba12091907
Andersonlh29 <Andersonlopez.alu@gmail.com>
2025-07-03 23:45:36 -0500

    Second commit

--- sketch.js
+++ sketch.js
@@ -18,43 +18,63 @@ let fuenteLED;
 let velocidadComputadora = 3;
 let seleccionNivel = true;
 
-let botonFacil, botonMedio, botonDificil;
+let botonFacil, botonMedio, botonDificil, botonReiniciar, botonPausa;
 
 let sonidoGolpe;
 let sonidoPublico;
 let sonidoFinal;
+
+let juegoPausado = false;
+
+// Sprites opcionales
+let spritePelota, spriteRaqueta;
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 490;
 
 function preload() {
   sonidoGolpe = loadSound('tennis-ball-hit-151257.mp3');
   sonidoPublico = loadSound('crowd-clapping-and-cheering-effect-272056.mp3');
   sonidoFinal = loadSound('fanfare-1-276819.mp3');
   fuenteLED = loadFont('https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/font/SourceCodePro-Bold.otf');
+
+  // OPCIONALES: Descomenta si tienes imágenes
+  // spritePelota = loadImage('pelota.png');
+  // spriteRaqueta = loadImage('raqueta.png');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(GAME_WIDTH, GAME_HEIGHT);
   textFont(fuenteLED);
-  crearBotones();
+  crearBotonesNivel();
   inicializarElementos();
+  crearBotonesJuego();
+  posicionarBotonesJuego();
 }
 
 function inicializarElementos() {
-  pelota = { x: width / 2, y: height / 2, diametro: 20, velocidadX: 5, velocidadY: 3 };
-  raquetaJugador = { x: 5, y: height / 2 - 40, ancho: 10, alto: 80 };
-  raquetaComputadora = { x: width - 15, y: height / 2 - 40, ancho: 10, alto: 80 };
+  pelota = {
+    x: GAME_WIDTH / 2,
+    y: GAME_HEIGHT / 2,
+    diametro: 20,
+    velocidadX: 5,
+    velocidadY: 3
+  };
+  raquetaJugador = { x: 5, y: GAME_HEIGHT / 2 - 40, ancho: 10, alto: 80 };
+  raquetaComputadora = { x: GAME_WIDTH - 15, y: GAME_HEIGHT / 2 - 40, ancho: 10, alto: 80 };
   marcoSuperior = { alto: 60 };
   marcoInferior = { alto: 10 };
   tiempoInicio = millis();
 }
 
-function crearBotones() {
-  let centroX = width / 2 - 40;
-  let centroY = height / 2 - 70;
+function crearBotonesNivel() {
+  let centroX = windowWidth / 2 - 40;
+  let centroY = windowHeight / 2 - 70;
 
   botonFacil = createButton("Fácil");
   botonFacil.position(centroX, centroY);
   botonFacil.size(80, 40);
-  botonFacil.style('background-color', '#228B22');
+  botonFacil.style('background-color', '#44e141');
   botonFacil.style('color', 'white');
   botonFacil.style('border', '2px solid white');
   botonFacil.mousePressed(() => seleccionarNivel(1));
@@ -62,7 +82,7 @@ function crearBotones() {
   botonMedio = createButton("Medio");
   botonMedio.position(centroX, centroY + 100);
   botonMedio.size(80, 40);
-  botonMedio.style('background-color', '#228B22');
+  botonMedio.style('background-color', '#44e141');
   botonMedio.style('color', 'white');
   botonMedio.style('border', '2px solid white');
   botonMedio.mousePressed(() => seleccionarNivel(2));
@@ -70,75 +90,145 @@ function crearBotones() {
   botonDificil = createButton("Difícil");
   botonDificil.position(centroX, centroY + 200);
   botonDificil.size(80, 40);
-  botonDificil.style('background-color', '#228B22');
+  botonDificil.style('background-color', '#44e141');
   botonDificil.style('color', 'white');
   botonDificil.style('border', '2px solid white');
   botonDificil.mousePressed(() => seleccionarNivel(3));
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function crearBotonesJuego() {
+  botonPausa = createButton("Pausar");
+  botonPausa.size(80, 30);
+  botonPausa.style('background-color', '#44e141');
+  botonPausa.style('color', 'white');
+  botonPausa.style('border', '2px solid white');
+  botonPausa.mousePressed(togglePausa);
+  botonPausa.hide();
 
+  botonReiniciar = createButton("Reiniciar");
+  botonReiniciar.size(80, 30);
+  botonReiniciar.style('background-color', '#44e141');
+  botonReiniciar.style('color', 'white');
+  botonReiniciar.style('border', '2px solid white');
+  botonReiniciar.mousePressed(reiniciarJuego);
+  botonReiniciar.hide();
+}
+
+function posicionarBotonesJuego() {
+  let cx = windowWidth / 2 - GAME_WIDTH / 2 + 20;
+  let cy = windowHeight / 2 - GAME_HEIGHT / 2 + 15;
+  botonPausa.position(cx, cy);
+  botonReiniciar.position(cx + 90, cy);
+}
+
+function windowResized() {
+  posicionarBotonesJuego();
   if (seleccionNivel) {
-    let centroX = width / 2 - 40;
-    let centroY = height / 2 - 70;
+    let centroX = windowWidth / 2 - 40;
+    let centroY = windowHeight / 2 - 70;
     botonFacil.position(centroX, centroY);
     botonMedio.position(centroX, centroY + 100);
     botonDificil.position(centroX, centroY + 200);
   }
-
-  raquetaComputadora.x = width - 15;
-  pelota.x = width / 2;
 }
 
 function seleccionarNivel(nivel) {
-  velocidadComputadora = nivel === 1 ? 1.5 : nivel === 2 ? 2.5 : 3;
+  velocidadComputadora = nivel === 1 ? 2 : nivel === 2 ? 3 : 4;
   seleccionNivel = false;
   botonFacil.hide();
   botonMedio.hide();
   botonDificil.hide();
+  botonPausa.show();
+  botonReiniciar.show();
   tiempoInicio = millis();
 }
 
+function reiniciarJuego() {
+  puntajeJugador = 0;
+  puntajeComputadora = 0;
+  juegoTerminado = false;
+  mensajeGanador = "";
+  inicializarElementos();
+  juegoPausado = false;
+  botonPausa.html("Pausar");
+  seleccionNivel = true;
+  crearBotonesNivel();
+  botonPausa.hide();
+  botonReiniciar.hide();
+  loop();
+}
+
+function togglePausa() {
+  if (!juegoPausado) {
+    noLoop();
+    juegoPausado = true;
+    botonPausa.html("Reanudar");
+  } else {
+    loop();
+    juegoPausado = false;
+    botonPausa.html("Pausar");
+  }
+}
+
 function draw() {
-  background(34, 139, 34);
+  background(0); // Fondo oscuro
+  push();
+  translate(windowWidth / 2 - GAME_WIDTH / 2, windowHeight / 2 - GAME_HEIGHT / 2);
+
+  fill(34, 139, 34);
+  rect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
   stroke(255);
   strokeWeight(4);
-  line(width / 2, marcoSuperior.alto, width / 2, height - marcoInferior.alto);
-  line(0, marcoSuperior.alto, width, marcoSuperior.alto);
-  line(0, height - marcoInferior.alto, width, height - marcoInferior.alto);
+  line(GAME_WIDTH / 2, marcoSuperior.alto, GAME_WIDTH / 2, GAME_HEIGHT - marcoInferior.alto);
+  line(0, marcoSuperior.alto, GAME_WIDTH, marcoSuperior.alto);
+  line(0, GAME_HEIGHT - marcoInferior.alto, GAME_WIDTH, GAME_HEIGHT - marcoInferior.alto);
+
   noStroke();
   fill(0);
-  rect(0, 0, width, marcoSuperior.alto);
+  rect(0, 0, GAME_WIDTH, marcoSuperior.alto);
+
   fill(0, 255, 0);
   textSize(18);
-  textAlign(CENTER, CENTER);
+  textAlign(LEFT, CENTER);
+
   let tiempoTranscurrido = int((millis() - tiempoInicio) / 1000);
   let minutos = nf(int(tiempoTranscurrido / 60), 2);
   let segundos = nf(tiempoTranscurrido % 60, 2);
-  text("TIEMPO", width / 2, 15);
-  text(`${minutos}:${segundos}`, width / 2, 35);
-  text(`Jugador: ${puntajeJugador}`, width / 4, 35);
+
+  text("TIEMPO", 250, 15);
+  text(`${minutos}:${segundos}`, 250, 35);
+  text(`Jugador: ${puntajeJugador}`, 380, 15);
+  text(`Computadora: ${puntajeComputadora}`, 380, 35);
+
   if (animacionPuntoJugador && millis() - tiempoAnimacionJugador < 1000) {
-    text("+1", width / 4 + 70, 35);
+    text("+1", 490, 15);
   }
-  text(`Computadora: ${puntajeComputadora}`, 3 * width / 4, 35);
   if (animacionPuntoComputadora && millis() - tiempoAnimacionComputadora < 1000) {
-    text("+1", 3 * width / 4 + 90, 35);
+    text("+1", 530, 35);
   }
+
   if (mostrarPunto && millis() - tiempoPunto < 1000) {
     textSize(48);
-    text("PUNTO!", width / 2, height / 2);
+    textAlign(CENTER, CENTER);
+    text("PUNTO!", GAME_WIDTH / 2, GAME_HEIGHT / 2);
   }
+
   if (juegoTerminado) {
     textSize(36);
     fill(255);
-    text(mensajeGanador, width / 2, height / 2 - 20);
-    text("Final del partido", width / 2, height / 2 + 20);
+    textAlign(CENTER, CENTER);
+    text(mensajeGanador, GAME_WIDTH / 2, GAME_HEIGHT / 2 - 20);
+    text("Final del partido", GAME_WIDTH / 2, GAME_HEIGHT / 2 + 20);
     noLoop();
+    pop();
     return;
   }
-  if (seleccionNivel) return;
+
+  if (seleccionNivel) {
+    pop();
+    return;
+  }
 
   if (puntajeJugador >= 5 && !juegoTerminado) {
     juegoTerminado = true;
@@ -151,30 +241,41 @@ function draw() {
   }
 
   dibujarPelota();
-
   pelota.x += pelota.velocidadX;
   pelota.y += pelota.velocidadY;
 
   if (pelota.y - pelota.diametro / 2 < marcoSuperior.alto) pelota.velocidadY *= -1;
-  if (pelota.y + pelota.diametro / 2 > height - marcoInferior.alto) pelota.velocidadY *= -1;
+  if (pelota.y + pelota.diametro / 2 > GAME_HEIGHT - marcoInferior.alto) pelota.velocidadY *= -1;
 
   dibujarRaqueta(raquetaJugador);
   dibujarRaqueta(raquetaComputadora);
 
   if (keyIsDown(UP_ARROW)) raquetaJugador.y -= 5;
   if (keyIsDown(DOWN_ARROW)) raquetaJugador.y += 5;
-  raquetaJugador.y = constrain(raquetaJugador.y, marcoSuperior.alto, height - marcoInferior.alto - raquetaJugador.alto);
+  raquetaJugador.y = constrain(raquetaJugador.y, marcoSuperior.alto, GAME_HEIGHT - marcoInferior.alto - raquetaJugador.alto);
 
-  if (pelota.y < raquetaComputadora.y + raquetaComputadora.alto / 2) raquetaComputadora.y -= velocidadComputadora;
-  else raquetaComputadora.y += velocidadComputadora;
-  raquetaComputadora.y = constrain(raquetaComputadora.y, marcoSuperior.alto, height - marcoInferior.alto - raquetaComputadora.alto);
+  // IA MEJORADA
+  let centroPelota = pelota.y;
+  let centroRaqueta = raquetaComputadora.y + raquetaComputadora.alto / 2;
+  if (centroPelota < centroRaqueta - 10) {
+    raquetaComputadora.y -= velocidadComputadora;
+  } else if (centroPelota > centroRaqueta + 10) {
+    raquetaComputadora.y += velocidadComputadora;
+  }
+  raquetaComputadora.y = constrain(raquetaComputadora.y, marcoSuperior.alto, GAME_HEIGHT - marcoInferior.alto - raquetaComputadora.alto);
 
+  // Rebotes realistas
   if (pelota.x - pelota.diametro / 2 < raquetaJugador.x + raquetaJugador.ancho && pelota.y > raquetaJugador.y && pelota.y < raquetaJugador.y + raquetaJugador.alto) {
     pelota.velocidadX *= -1;
+    let impacto = (pelota.y - (raquetaJugador.y + raquetaJugador.alto / 2)) / (raquetaJugador.alto / 2);
+    pelota.velocidadY = 5 * impacto;
     sonidoGolpe.play();
   }
+
   if (pelota.x + pelota.diametro / 2 > raquetaComputadora.x && pelota.y > raquetaComputadora.y && pelota.y < raquetaComputadora.y + raquetaComputadora.alto) {
     pelota.velocidadX *= -1;
+    let impacto = (pelota.y - (raquetaComputadora.y + raquetaComputadora.alto / 2)) / (raquetaComputadora.alto / 2);
+    pelota.velocidadY = 5 * impacto;
     sonidoGolpe.play();
   }
 
@@ -187,7 +288,7 @@ function draw() {
     tiempoAnimacionComputadora = millis();
     if (puntajeComputadora < 5) sonidoPublico.play();
   }
-  if (pelota.x > width) {
+  if (pelota.x > GAME_WIDTH) {
     puntajeJugador++;
     pelota.velocidadX *= -1;
     mostrarPunto = true;
@@ -196,21 +297,32 @@ function draw() {
     tiempoAnimacionJugador = millis();
     if (puntajeJugador < 5) sonidoPublico.play();
   }
+
+  pop();
 }
 
 function dibujarRaqueta(raqueta) {
-  fill(255);
-  ellipse(raqueta.x + raqueta.ancho / 2, raqueta.y + raqueta.alto / 3, raqueta.ancho * 4, raqueta.alto / 1.5);
-  rect(raqueta.x + raqueta.ancho / 2 - 2, raqueta.y + raqueta.alto / 3, 4, raqueta.alto / 1.8);
+  if (spriteRaqueta) {
+    image(spriteRaqueta, raqueta.x, raqueta.y, raqueta.ancho * 4, raqueta.alto);
+  } else {
+    fill(255);
+    ellipse(raqueta.x + raqueta.ancho / 2, raqueta.y + raqueta.alto / 3, raqueta.ancho * 4, raqueta.alto / 1.5);
+    rect(raqueta.x + raqueta.ancho / 2 - 2, raqueta.y + raqueta.alto / 3, 4, raqueta.alto / 1.8);
+  }
 }
 
 function dibujarPelota() {
-  fill(173, 255, 47);
-  ellipse(pelota.x, pelota.y, pelota.diametro);
-  stroke(255);
-  strokeWeight(2);
-  noFill();
-  arc(pelota.x, pelota.y, pelota.diametro, pelota.diametro, PI / 4, PI + PI / 4);
-  arc(pelota.x, pelota.y, pelota.diametro, pelota.diametro, -PI / 4, PI - PI / 4);
-  noStroke();
+  if (spritePelota) {
+    image(spritePelota, pelota.x - 10, pelota.y - 10, 20, 20);
+  } else {
+    fill(173, 255, 85);
+    ellipse(pelota.x, pelota.y, pelota.diametro);
+    stroke(255);
+    strokeWeight(2);
+    noFill();
+    arc(pelota.x, pelota.y, pelota.diametro, pelota.diametro, PI / 4, PI + PI / 4);
+    arc(pelota.x, pelota.y, pelota.diametro, pelota.diametro, -PI / 4, PI - PI / 4);
+    noStroke();
+  }
 }
+
